885929bb25316d853f3efcc1b50f72b0b985f860
80975117 <80975117@telefonicati.onmicrosoft.com>
2025-05-29 01:38:33 -0300

    fix: update Agro Dash project title, description and metadata

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,20 +1,21 @@
-const title = 'React PWA';
+const title = 'Agro Dash';
 
-const email = 'auther-email@gmail.com';
+const email = 'philipeloureiro@gmail.com';
 
-const repository = 'https://github.com/suren-atoyan/react-pwa';
+const repository = 'https://github.com/philoureiro/agro-dash';
 
 const dateFormat = 'MMMM DD, YYYY';
 
 const loader = {
   // no more blinking in your app
-  delay: 300, // if your asynchronous process is finished during 300 milliseconds you will not see the loader at all
-  minimumLoading: 700, // but if it appears, it will stay for at least 700 milliseconds
+  delay: 300, // if seu processo assíncrono acabar em 300ms, nem mostra loader
+  minimumLoading: 700, // mas se aparecer, fica pelo menos 700ms
 };
 
 const defaultMetaTags = {
-  image: '/cover.png',
-  description: 'Starter kit for modern web applications',
+  image: '/icon-512.png', // Use o ícone do projeto ou um cover custom
+  description:
+    'Dashboard inteligente para gestão de propriedades rurais, acompanhamento de produtores e visualização de dados agrícolas em tempo real.',
 };
 
 export { loader, dateFormat, repository, email, title, defaultMetaTags };
